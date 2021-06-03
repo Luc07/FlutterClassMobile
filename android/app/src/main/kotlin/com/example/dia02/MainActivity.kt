8ce8a25b74f56e6f6f35e0897ea34f22646406fd
Lucas Emmanuel <lucas1822@outlook.com>
2021-06-02 23:39:14 -0300

    Flutter Planets App

---
+++ android/app/src/main/kotlin/com/example/dia02/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.dia02
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/homePage.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'gradientAppBar.dart';
+import 'homePageBody.dart';
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      body: new Column(
+        children: <Widget>[
+          new GradientAppBar("Treva"),
+          new HomePageBody(),
+        ],
+      ),
+    );
+  }
+}
